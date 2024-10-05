f0d52bf27433457c43dae2b657603b682235dde4
struo288 <78322463+simont-17@users.noreply.github.com>
2024-10-05 11:40:34 -0600

    Got the routing for pages working

--- mruhack-2024/src/App.jsx
+++ mruhack-2024/src/App.jsx
@@ -2,11 +2,19 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 
+import Home from './pages/Home'
+import About from './pages/About'
+import Nav from './components/Nav'
+
 function App() {
 
   return (
     <Router>
-      
+      <Nav/>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+      </Routes>
     </Router>
   )
 }
