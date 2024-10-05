f37a8c0f15f580211feb02000e5b016b5d1c85b0
struo288 <78322463+simont-17@users.noreply.github.com>
2024-10-05 11:22:13 -0600

    Start of vite + react project

---
+++ mruhack-2024/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
