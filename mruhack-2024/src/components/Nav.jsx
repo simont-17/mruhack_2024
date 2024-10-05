4467a17d793b12b3b02fcefbcc159fc38ecf1c37
struo288 <78322463+simont-17@users.noreply.github.com>
2024-10-05 12:53:06 -0600

    Links to nav added

--- mruhack-2024/src/components/Nav.jsx
+++ mruhack-2024/src/components/Nav.jsx
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom';
+
 export default function Nav() {
-    return(
-        <>
+    return (
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
             <p>Test</p>
-        </>
+        </nav>
     )
 }
