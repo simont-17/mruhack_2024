0dfed31068f831f5fa8242857b5369a565c46ad4
struo288 <78322463+simont-17@users.noreply.github.com>
2024-10-06 10:26:22 -0600

    Added about content, styling and images

--- mruhack-2024/src/pages/About.jsx
+++ mruhack-2024/src/pages/About.jsx
@@ -1,7 +1,45 @@
 export default function About() {
     return(
-        <>
-            <p>About test</p>
-        </>
+        <div className="about-container">
+            <section className="mission-section">
+                <h1>Our Mission</h1>
+                <p>Have you ever procastinated a project just because the size seemed too daunting and you just don't know how to even start? With Projectvitiy, you can enter your assignemnt instructions and have a fully automated generation of a breakdown of steps, including when to start and when you should have it dne by.</p>
+                <p>According to American Psychological Association (APA), 80 to 95% of university students procastinate. To challenge this statistic, we built Projectivity with university students mind and what kind of struggles we anecotcally go through.</p>
+            </section>
+            <section className="team-section">
+                <h1>Our Team</h1>
+                <div className="team-container">
+                    <div class="team-member">
+                        <img src="team-dan.jpg" width="200px" height="200px"></img>
+                        <h2>Dan Nguyen</h2>
+                        <p>Software Engineer</p>
+                    </div>
+                    <div class="team-member">
+                        <img src="team-simon.jpg" width="200px" height="200px"></img>
+                        <h2>Simon Truong</h2>
+                        <p>Software Engineer</p>
+                    </div>
+                    <div class="team-member">
+                        <img src="team-zaarif.JPG" width="200px" height="200px"></img>
+                        <h2>Zaarif Sardar</h2>
+                        <p>Software Engineer</p>
+                    </div>
+                    <div class="team-member">
+                        <img src="team-armandeep.JPG" width="200px" height="200px"></img>
+                        <h2>Armandeep Grewal</h2>
+                        <p>Software Engineer</p>
+                    </div>
+                    <div class="team-member">
+                        <img src="team-nathan.JPG" width="200px" height="200px"></img>
+                        <h2>Nathan Phan</h2>
+                        <p>Software Engineer</p>
+                    </div>
+
+                </div>
+
+            </section>
+        </div>
     )
 }
+
+ 
